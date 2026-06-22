aa17a81d1e878eb51c71a56a04587a00b3447a84
Daniil Halushka <92893370+DaniilHalushka@users.noreply.github.com>
2026-06-22 21:25:26 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] countArray = new int[26];
+
+        for (int i = 0; i <= s.length() - 1; i++) {
+            countArray[s.charAt(i) - 'a']++;
+            countArray[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i <= countArray.length - 1; i++) {
+            if (countArray[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
