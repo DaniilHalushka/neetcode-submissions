553f6262df53f532e11377d2e2a8354ab362a1ec
Daniil Halushka <92893370+DaniilHalushka@users.noreply.github.com>
2026-06-24 23:27:31 +0300

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        //if we need to keep the original relative order of elements
+        if (nums.length == 0) return 0;
+
+        int write = 0;
+
+        for (int read = 0; read < nums.length; read++) {
+            if (nums[read] != val) {
+                nums[write] = nums[read];
+                write++;
+            }
+        }
+
+        return write;
+        
+    }
+}
